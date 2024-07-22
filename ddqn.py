adding qval running average of fixed states (from paper) to final plots

ddqn.py
@@ -50,11 +50,11 @@ class DeepQNetwork(torch.nn.Module):
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
-                init.orthogonal_(m.weight)
+                torch.nn.init.orthogonal_(m.weight)
                 if isinstance(m, torch.nn.Linear):
-                    m.weight.data.mul_(1/100)
+                    m.weight.data.mul_(1 / 100)
                 if m.bias is not None:
-                    init.constant_(m.bias, 0)
+                    torch.nn.init.constant_(m.bias, 0)
 
 
 class DDQNAgent:

main.py
@@ -15,6 +15,28 @@ warnings.simplefilter("ignore")
 ALEInterface.setLoggerMode(LoggerMode.Error)
 
 
+def collect_fixed_states(env_name, n_envs=50, max_steps=10):
+    def make_env():
+        return AtariEnv(
+            env_name,
+            shape=(84, 84),
+            repeat=4,
+            clip_rewards=True,
+        ).make()
+
+    envs = gym.vector.AsyncVectorEnv([make_env for _ in range(n_envs)])
+    states, _ = envs.reset()
+
+    steps = np.random.randint(1, max_steps)
+    for _ in range(steps):
+        actions = [envs.single_action_space.sample() for _ in range(n_envs)]
+        states, _, term, trunc, _ = envs.step(actions)
+        if term.any() or trunc.any():
+            break
+
+    return np.array(states)
+
+
 def run_ddqn(args):
 
     def make_env():
@@ -51,6 +73,8 @@ def run_ddqn(args):
     score = np.zeros(args.n_envs)
     history, metrics = [], []
 
+    fixed_states = collect_fixed_states(args.env, n_envs=50)
+
     states, _ = envs.reset()
     for i in range(args.n_steps):
         actions = [agent.choose_action(state) for state in states]
@@ -81,11 +105,14 @@ def run_ddqn(args):
             best_score = avg_score
             agent.save_checkpoint()
 
+        avg_q_value = np.mean([agent.q1(state).max().item() for state in fixed_states])
+
         metrics.append(
             {
                 "episode": i + 1,
                 "average_score": avg_score,
                 "best_score": best_score,
+                "average_q_value": avg_q_value,
             }
         )
 
@@ -103,7 +130,7 @@ def run_ddqn(args):
 
 def save_results(env_name, history, metrics, agent):
     save_prefix = env_name.split("/")[-1]
-    utils.plot_running_avg(history, save_prefix)
+    utils.plot_running_avg(history, save_prefix, metrics)
     df = pd.DataFrame(metrics)
     df.to_csv(f"metrics/{save_prefix}_metrics.csv", index=False)
     save_best_version(env_name, agent)

utils.py
@@ -38,14 +38,20 @@ def save_animation(frames, filename):
             writer.append_data(frame)
 
 
-def plot_running_avg(scores, env):
-    avg = np.zeros_like(scores)
+def plot_running_avg(scores, env, metrics):
+    avg_scores = np.zeros_like(scores)
+    avg_q_values = [m["average_q_value"] for m in metrics]
+
     for i in range(len(scores)):
-        avg[i] = np.mean(scores[max(0, i - 100) : i + 1])
-    plt.plot(avg)
-    plt.title("Running Average per 100 Games")
+        avg_scores[i] = np.mean(scores[max(0, i - 100) : i + 1])
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(avg_scores, label="Average Score")
+    plt.plot(avg_q_values, label="Average Q Value")
+    plt.title("Running Average per 100 Games and Average Q Value")
     plt.xlabel("Episode")
-    plt.ylabel("Average Score")
+    plt.ylabel("Value")
+    plt.legend()
     plt.grid(True)
-    plt.savefig(f"metrics/{env}_running_avg.png")
+    plt.savefig(f"metrics/{env}_running_avg_q.png")
     plt.close()